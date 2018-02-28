b3fe82f46e435c750381677c5a18128a669ecc05
quoctrungdh <quoctrungdh@gmail.com>
2018-02-28 14:35:36 +0700

    refactor structure code + eslint

---
+++ client/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Nav from './common/Nav';
+import Home from './routes/Home';
+import About from './routes/About';
+import NotFound from './routes/NotFound';
+
+import './style.scss';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
+
+
+/**
+ * TODO: ESLINT
+ * REDUX
+ * STORY-BOOK
+ * UNIT TEST
+ */
