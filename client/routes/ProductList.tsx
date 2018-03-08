2cc90dd39421697a0e6f51ad06130db968e86a20
lamanhdai <lanhdai1988@gmail.com>
2018-03-08 22:50:29 +0700

    feat(SHOPPY-PRODUCTLIST): update css, dom

--- client/routes/ProductList.tsx
+++ client/routes/ProductList.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
+import * as ProductImage1 from '../assets/images/shoes1.png';
+import * as ProductImage2 from '../assets/images/shoes2.png';
+import * as ProductImage3 from '../assets/images/shoes3.png';
+import * as ProductImage4 from '../assets/images/shoes4.png';
+import * as ProductImage5 from '../assets/images/shoes5.png';
+import * as ProductImage6 from '../assets/images/shoes6.png';
+import * as FavoriteIcon from '../assets/images/favorite_icon.png';
+import * as FavoriteActiveIcon from '../assets/images/favorite_active_icon.png';
+import * as PackgageIcon from '../assets/images/package_icon.png';
+import * as PackgageActiveIcon from '../assets/images/package_active_icon.png';
+import { Link } from 'react-router-dom';
 
 interface Product {
   productId:string;
   name:string;
   price:number;
   isFavorite:boolean;
+  inPackage:boolean;
   imageUrl:string;
   sizes:number[];
   colors:string[];
@@ -16,10 +28,11 @@ const productList = [
   {
     productId: "0001",
     name: "nike-01",
-    price: 300,
-    isFavorite: true,
-    imageUrl: '../assets/images/shoes1.png',
-    sizes: ["42", "43", "44", "45"],
+    price: 25678,
+    isFavorite: false,
+    inPackage: true,
+    imageUrl: ProductImage1,
+    sizes: [42, 43, 44, 45],
     colors: ["red", "gray", "pink", "black"],
     description: "fdshksdhfkdshfsd fhdsskjjfhsdk fdhskfh fdshsfkjdjhskfbdsk dfsshfkjdjshf",
     type: "shoes"
@@ -27,10 +40,11 @@ const productList = [
   {
     productId: "0002",
     name: "nike-02",
-    price: 300,
+    price: 25678,
     isFavorite: true,
-    imageUrl: '',
-    sizes: ["42", "43", "44", "45"],
+    inPackage: false,
+    imageUrl: ProductImage2,
+    sizes: [42, 43, 44, 45],
     colors: ["red", "gray", "pink", "black"],
     description: "fdshksdhfkdshfsd fhdsskjjfhsdk fdhskfh fdshsfkjdjhskfbdsk dfsshfkjdjshf",
     type: "shoes"
@@ -38,10 +52,11 @@ const productList = [
   {
     productId: "0003",
     name: "nike-03",
-    price: 300,
+    price: 25678,
     isFavorite: true,
-    imageUrl: '',
-    sizes: ["42", "43", "44", "45"],
+    inPackage: false,
+    imageUrl: ProductImage3,
+    sizes: [42, 43, 44, 45],
     colors: ["red", "gray", "pink", "black"],
     description: "fdshksdhfkdshfsd fhdsskjjfhsdk fdhskfh fdshsfkjdjhskfbdsk dfsshfkjdjshf",
     type: "shoes"
@@ -49,10 +64,11 @@ const productList = [
   {
     productId: "0004",
     name: "nike-04",
-    price: 300,
+    price: 25678,
     isFavorite: true,
-    imageUrl: '',
-    sizes: ["42", "43", "44", "45"],
+    inPackage: true,
+    imageUrl: ProductImage4,
+    sizes: [42, 43, 44, 45],
     colors: ["red", "gray", "pink", "black"],
     description: "fdshksdhfkdshfsd fhdsskjjfhsdk fdhskfh fdshsfkjdjhskfbdsk dfsshfkjdjshf",
     type: "shoes"
@@ -60,10 +76,11 @@ const productList = [
   {
     productId: "0005",
     name: "nike-05",
-    price: 300,
+    price: 25678,
     isFavorite: true,
-    imageUrl: '',
-    sizes: ["42", "43", "44", "45"],
+    inPackage: true,
+    imageUrl: ProductImage5,
+    sizes: [42, 43, 44, 45],
     colors: ["red", "gray", "pink", "black"],
     description: "fdshksdhfkdshfsd fhdsskjjfhsdk fdhskfh fdshsfkjdjhskfbdsk dfsshfkjdjshf",
     type: "shoes"
@@ -71,10 +88,11 @@ const productList = [
   {
     productId: "0006",
     name: "nike-06",
-    price: 300,
+    price: 25678,
     isFavorite: true,
-    imageUrl: '',
-    sizes: ["42", "43", "44", "45"],
+    inPackage: true,
+    imageUrl: ProductImage6,
+    sizes: [42, 43, 44, 45],
     colors: ["red", "gray", "pink", "black"],
     description: "fdshksdhfkdshfsd fhdsskjjfhsdk fdhskfh fdshsfkjdjhskfbdsk dfsshfkjdjshf",
     type: "shoes"
@@ -115,22 +133,24 @@ export default class ProductList extends React.Component<{}, {}> {
         <h1>ProductList</h1>
         <div className="flex-row">
           {
-            this.state.error && 
+            this.state.error &&
             <p>{this.state.error.message}</p>
           }
           {
-            this.state.isLoading && 
+            this.state.isLoading &&
             <p>...loading</p>
           }
           {
             this.state.productsList.map((item:Product) => (
-              <div className="flex-item" key={item.productId}>
-                <p className="product__type">{item.type}</p>
-                <h3 className="product__name">{item.name}</h3>
-                <img src="item.imageUrl" alt="" className="product__image" />
-                <p className="product__price">{item.price}</p>
-                <img src="{item.isFavorite ? '../assets/images/like_active_icon.png' : '../assets/images/like_icon.png'}" alt="" className="product__like-status" />
-                <button type="button"><img src="../assets/images/bag_icon.png" alt="" className="product__cart-button" /></button>
+              <div className="flex-item text-center" key={item.productId}>
+                <div className="product__box">
+                  <p className="product__type">{item.type}</p>
+                  <h3 className="product__name">{item.name}</h3>
+                  <img src={item.imageUrl} alt="Product lists" className="product__image" />
+                  <p className="product__price">${item.price}</p>
+                  <Link to="" className="product__favorite-icon"><img src={item.isFavorite ? FavoriteActiveIcon : FavoriteIcon} alt="Favorite icon " /></Link>
+                  <Link to="" className="product__package-icon"><img src={item.inPackage ? PackgageActiveIcon : PackgageIcon} alt="Package icon" /></Link>
+                </div>
               </div>
             ))
           }
